d78615071dd63ed0d82ada82fc9db1917418365c
Mahesh3317 <maheshsharma3317js@gmail.com>
2025-05-31 11:01:54 +0530

    netlify

---
+++ pages/blog/[slug].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogPost = () => {
+  return <div>Blog post content goes here</div>;
+};
+
+export default BlogPost;
---
+++ pages/inquiry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Inquiry = () => {
+  return <div>Inquiry form goes here</div>;
+};
+
+export default Inquiry;
